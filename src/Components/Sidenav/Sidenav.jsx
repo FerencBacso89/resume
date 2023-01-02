d14439a7276b7d5eb5ecab375472eae9e50f0ab8
fbacso <50657103+FerencBacso89@users.noreply.github.com>
2023-01-02 13:51:57 +0100

    nav icons & tooltip added
    Material ui npm install

--- src/Components/Sidenav/Sidenav.jsx
+++ src/Components/Sidenav/Sidenav.jsx
@@ -5,6 +5,28 @@ import logo from "../../assets/img/bg-goose.png"
 import "../Sidenav/sidenav.css"
 // import { BootstrapIcon } from 'react-icons/bs'
 // import { BsHouseDoorFill } from "react-icons/bs/BsHouseDoorFill"
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import { IconButton } from '@mui/material';
+import { styled } from "@mui/material/styles";
+import {
+  BsPersonSquare,
+  BsCardChecklist,
+  BsPersonLinesFill,
+  BsJournalCode,
+} from "react-icons/bs";
+ 
+const TooltipIcon = styled(({ className, ...props }) => (
+    <Tooltip {...props} arrow classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.arrow}`]: {
+      color: theme.palette.common.black,
+    },
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: theme.palette.common.black,
+      fontSize: "0.8rem",
+      fontWeight: "bold",
+    },
+  }));
 
 function Sidenav() {
   return (
@@ -15,6 +37,13 @@ function Sidenav() {
         {/* <NavLink exact="true" activeclassname="active" to="/">
               <BootstrapIcon icon={BsHouseDoorFill} color="#434242"/>
         </NavLink> */}
+        <NavLink className={"link-elem"}>
+          <TooltipIcon title="Introducion" arrow placement="right-start">
+            <IconButton className="navElement">
+              <BsPersonLinesFill />
+            </IconButton>
+          </TooltipIcon>
+        </NavLink>
     </div>
   )
 }
