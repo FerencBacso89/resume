72b6ad410fc6a7b5a6484fbbaa3dcd7e791b260e
Bacsó Ferenc <50657103+FerencBacso89@users.noreply.github.com>
2023-06-27 22:02:06 +0200

    welome page
    profile img left & text right

--- src/Components/Home/Welcome.jsx
+++ src/Components/Home/Welcome.jsx
@@ -6,18 +6,20 @@ import './welcome.css';
 
 function Welcome() {
   return (
-    <div className='mindset'>
-        <img id="myImg" alt='Profile Picture' src={ProfilImg}></img>
-        <div>
-            <h2 className='mainTitle'>Welcome to My portfolio site</h2>
-            <article id='mindsetText'>
-            My name is Ferenc Bacsó, and I have been working as a frontend developer since 2020. I am thrilled to share my work and passion for web development with you.
-              Currently, I am employed at the National Directorate General for Disaster Management, where I am dedicatedly involved in various IT projects. In 2022, I was recognized for my outstanding contributions in the field, an achievement that I am truly proud of.
-              However, my life is not solely focused on coding and technology. I have a deep love for birds and birdwatching, which brings me tranquility and inspiration. Hence, I decided to incorporate a flying goose logo, symbolizing my constant pursuit of growth and improvement.
-              On this website, I showcase my projects and work that I have been involved in over the years. I proudly present my latest frontend development accomplishments and endeavors, as well as the creative solutions I have found for unique challenges.
-              I am more than happy to answer any questions or explore potential collaboration opportunities you may have. Thank you for visiting, and I hope you find the inspiration and interest that I aimed to convey on my website.
-            </article>
+    <div className='mindset container-fluid'>
+      <div className='row'>
+          <div className='col-3 imgPos' ><img id="myImg" className=' img-fluid' alt='Profile Picture' src={ProfilImg}></img></div>
+          <div className='col-6 Textpos'>
+                <article id='mindsetText'>
+                <h2 className='mainTitle'>Welcome to My portfolio site</h2>
+                My name is Ferenc Bacsó, and I have been working as a frontend developer since 2020.
+                <p>I am thrilled to share my work and passion for web development with you.
+                  In 2022, I was recognized for my outstanding contributions in the field, an achievement that I am truly proud of.</p> 
+                  However, my life is not solely focused on coding and technology. I have a deep love for birds and birdwatching, which brings me tranquility and inspiration. Hence, I decided to incorporate a flying goose logo, symbolizing my constant pursuit of growth and improvement. On this website, I showcase my projects and work that I have been involved. I proudly present my latest frontend development accomplishments and endeavors.
+                <p>Thank you for visiting, and I hope you find the inspiration and interest that I aimed to convey on my website.</p>
+                </article>
           </div>
+      </div>
     </div>
   )
 }
