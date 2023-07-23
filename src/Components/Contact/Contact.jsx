5f97e2be2f93fd52a69def2298f2e8586e36c543
Bacsó Ferenc <50657103+FerencBacso89@users.noreply.github.com>
2023-07-23 14:57:36 +0200

    position contqct info

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,12 +7,13 @@ function Contact() {
   return (
     <>
     <h2 className='container-fluid conTitle' >Contact me</h2>
-    <section className='contacts col-10'>
-           <span className='row'> email: </span>
-           <span className='row'> visit my Github : <GitHub></GitHub> </span>
-           <span className='row'> visit my Sandbox for some snipets : <SiCodesandbox></SiCodesandbox> </span>
-           <span className='row'> visit my LinkedIn : <LinkedIn></LinkedIn> </span>
-           <span className='row'> Download my CV :  </span>
+    <section className='contacts col-10 row-cols-2'>
+           <div className='cline col'> email: ferenc.bacso89 at gmail.com</div>
+           <div className='cline col'>  </div>
+           <span className='cline col'> visit my Github : <GitHub></GitHub> </span>
+           <span className='cline col'> visit my Sandbox for some snipets : <SiCodesandbox></SiCodesandbox> </span>
+           <span className='cline col'> visit my LinkedIn : <LinkedIn></LinkedIn> </span>
+           <span className='cline col'> Download my CV :  </span>
     </section>
     </>
   )
