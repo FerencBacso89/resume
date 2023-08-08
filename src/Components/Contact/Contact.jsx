e3c821cea8e9200d07b9e29dcaf2e70aa4f412b7
Bacsó Ferenc <50657103+FerencBacso89@users.noreply.github.com>
2023-08-08 16:52:45 +0200

    contact info has been added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,9 +10,9 @@ function Contact() {
     <section className='contacts col-10 row-cols-2'>
            <div className='cline col'> email: ferenc.bacso89 at gmail.com</div>
            <div className='cline col'>  </div>
-           <span className='cline col'> visit my Github : <GitHub></GitHub> </span>
+          {/* <span className='cline col'> visit my Github : <GitHub></GitHub> </span>
            <span className='cline col'> visit my Sandbox for some snipets : <SiCodesandbox></SiCodesandbox> </span>
-           <span className='cline col'> visit my LinkedIn : <LinkedIn></LinkedIn> </span>
+           <span className='cline col'> visit my LinkedIn : <LinkedIn></LinkedIn> </span> */}
            <span className='cline col'> Download my CV :  </span>
     </section>
     </>
