74fbf6eebd3ee551cb9ce172a88b307a25bea294
Bacsó Ferenc <50657103+FerencBacso89@users.noreply.github.com>
2023-09-10 14:13:31 +0200

    updated dowloadable cv and reference
    added cv eng version

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -17,14 +17,25 @@ const HandleGit=()=>{
 const HandleSandbox=()=>{
   window.open("https://codesandbox.io/u/FerencBacso89","_blank")
 }
-const getCV =()=>{
-  const link = document.createElement('a');
-  link.href = '../src/assets/CV/Bacsó-Ferenc_CV-HUN.pdf'; // Az elérési út a PDF fájlhoz
-  link.download = 'Bacsó-Ferenc_CV-HUN.pdf'; // A letöltött fájl neve
-  link.target = '_blank';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+const getCV =(event)=>{
+  if(event.target.id == "hunCV"){
+    const link = document.createElement('a');
+    link.href = '../src/assets/CV/Bacsó-Ferenc_CV-HUN.pdf'; // Az elérési út a PDF fájlhoz
+    link.download = 'Bacsó-Ferenc_CV-HUN.pdf'; // A letöltött fájl neve
+    link.target = '_blank';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    }
+    if(event.target.id == "engCV"){
+      const link = document.createElement('a');
+      link.href = '../src/assets/CV/Bacsó-Ferenc_CV-ENG.pdf'; // Az elérési út a PDF fájlhoz
+      link.download = 'Bacsó-Ferenc_CV-ENG.pdf'; // A letöltött fájl neve
+      link.target = '_blank';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
 }
 function Contact() {
   return (
@@ -36,10 +47,10 @@ function Contact() {
            <div className='cline col'> <b>E-mail: </b>ferenc.bacso89 <img className='atSymbol' src={at}></img> gmail.com </div>
            <div className=' col'> <SiLinkedin onClick={HandleLinkedin} className='Contactico'></SiLinkedin> <SiGithub onClick={HandleGit} className='Contactico'></SiGithub> <SiCodesandbox onClick={HandleSandbox} className='Contactico'></SiCodesandbox></div>
            <div className='cline col'> <b>Download CV: </b> <p> 
-           <Button className='CVbtn' onClick={getCV} sx={{ mt: 1, mr: 1 }}>
+           <Button id='hunCV' className='CVbtn' onClick={getCV} sx={{ mt: 1, mr: 1 }}>
             Download my CV <img className='cvFlag' src={Hunflag}></img> version
           </Button>          
-          <Button className='CVbtn' onClick={getCV} sx={{ mt: 1, mr: 1 }}>
+          <Button id='engCV' className='CVbtn' onClick={getCV} sx={{ mt: 1, mr: 1 }}>
             Download my CV <img className='cvFlag' src={Enflag}></img> version
           </Button>
           </p></div>
--- src/Components/Educate/Educate.jsx
+++ src/Components/Educate/Educate.jsx
@@ -85,7 +85,8 @@ export default function VerticalLinearStepper() {
   const handleReset = () => {
     setActiveStep(0);
   };
-  const getCV =()=>{
+  const getCV =(event)=>{
+    if(event.target.id == "hunCV"){
     const link = document.createElement('a');
     link.href = '../src/assets/CV/Bacsó-Ferenc_CV-HUN.pdf'; // Az elérési út a PDF fájlhoz
     link.download = 'Bacsó-Ferenc_CV-HUN.pdf'; // A letöltött fájl neve
@@ -93,6 +94,16 @@ export default function VerticalLinearStepper() {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+    }
+    if(event.target.id == "engCV"){
+      const link = document.createElement('a');
+      link.href = '../src/assets/CV/Bacsó-Ferenc_CV-ENG.pdf'; // Az elérési út a PDF fájlhoz
+      link.download = 'Bacsó-Ferenc_CV-ENG.pdf'; // A letöltött fájl neve
+      link.target = '_blank';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
   }
 
   return (
@@ -142,10 +153,10 @@ export default function VerticalLinearStepper() {
             Reset
           </Button>
           <Typography>If you would like to read more about me, click the button below for my resume</Typography>
-          <Button onClick={getCV} sx={{ mt: 1, mr: 1 }}>
+          <Button id='hunCV' onClick={getCV} sx={{ mt: 1, mr: 1 }}>
             Download my CV <img className='cvFlag' src={Hunflag}></img> version
           </Button>          
-          <Button onClick={getCV} sx={{ mt: 1, mr: 1 }}>
+          <Button id='engCV' onClick={getCV} sx={{ mt: 1, mr: 1 }}>
             Download my CV <img className='cvFlag' src={Enflag}></img> version
           </Button>
         </Paper>
